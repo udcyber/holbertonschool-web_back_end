027c2caa791070b8487631a62802996c73e051aa
udcyber <udewaleyne.pro@gmail.com>
2026-06-19 13:55:09 +0200

    alter code

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
 Take the code from wait_n and alter it into a new function task_wait_n
+The code is nearly identical to wait_n except task_wait_random
+is being called
 """
 import asyncio
 from typing import List
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Alter code from wait_n into task_wait_n.
+    Spawn task_wait_random n times with the specified max_delay
     """
     task_wait_random = __import__("3-tasks").task_wait_random
     delay_list = [task_wait_random(max_delay) for i in range(n)]
